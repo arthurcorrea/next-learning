3619d6ee2a43d64a41317f07325b1cb654efe6db
ArthurCorrea <arthur.costa@versatecnologia.com.br>
2025-02-12 12:12:13 -0300

    correçãoooo

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,4 +2,4 @@ import { Inter, Lusitana } from 'next/font/google';
 
 export const inter = Inter({ weight: '400' });
 
-export const lusitana = Lusitana({ weight: '400' });
+export const lusitana = Lusitana({ subsets: ['latin'], weight: '400' });
